e4e0e1d09bd169e61c201276b891e1a13badb0ce
yongsin0129 <yongsin0129@gmail.com>
2023-08-28 14:56:39 +0800

    feat : learn many2one example

--- addons/twturbiks_starter/__manifest__.py
+++ addons/twturbiks_starter/__manifest__.py
@@ -15,7 +15,7 @@
     "category": "Uncategorized",
     "version": "0.1",
     # any module necessary for this one to work correctly
-    "depends": ["base"],
+    "depends": ["base", "hr_contract"],
     # always loaded
     "data": [
         "security/ir.model.access.csv",
--- addons/twturbiks_starter/models/models.py
+++ addons/twturbiks_starter/models/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class twturbiks_starter(models.Model):
@@ -11,6 +11,21 @@ class twturbiks_starter(models.Model):
     value = fields.Integer()
     value2 = fields.Float(compute="_value_pc", store=True)
     description = fields.Text()
+    """
+    tutorial 1
+
+    default=lambda self: self.env.user
+    這行會在 obj 建立的時候，預設是現在的 user 身份
+    """
+    user_id = fields.Many2one("res.users", default=lambda self: self.env.user)
+
+    """
+    tutorial 2
+
+    做關連之前需要先確認 table 是否存在
+    manifest 的 depends 中要加入 hr_contract 
+    """
+    employee_id = fields.Many2one("hr.employee", string="Employee")
 
     @api.depends("value")
     def _value_pc(self):
