e5a0dbd9f945bf7e7f4dfe4a617af534d198d0b0
yongsin0129 <yongsin0129@gmail.com>
2023-08-28 15:52:04 +0800

    feat : add selection field note

--- addons/twturbiks_starter/models/models.py
+++ addons/twturbiks_starter/models/models.py
@@ -11,12 +11,14 @@ class twturbiks_starter(models.Model):
     value = fields.Integer()
     value2 = fields.Float(compute="_value_pc", store=True)
     description = fields.Text()
+
     """
     tutorial 1 Many2one
 
     default=lambda self: self.env.user
     這行會在 obj 建立的時候，預設是現在的 user 身份
     """
+
     user_id = fields.Many2one("res.users", default=lambda self: self.env.user)
 
     """
@@ -25,15 +27,14 @@ class twturbiks_starter(models.Model):
     做關連之前需要先確認 table 是否存在
     manifest 的 depends 中要加入 hr_contract 
     """
+
     employee_id = fields.Many2one("hr.employee", string="Employee")
 
     """
     tutorial 3 Many2Many
 
-    1. 建立 Many2many 之前, 一定要先定義一個 model !!
-    2. security 記得要建立
-
-    常見的例子 : tag 
+    1. 建立 Many2many 之前, 一定要先定義一個 model !! (本例子使用 DemoTag )
+    2. security/ir.model.access.csv 記得要設定
 
     https://www.odoo.com/documentation/master/developer/reference/backend/orm.html#odoo.fields.Many2many
     參數說明 : 
@@ -47,7 +48,27 @@ class twturbiks_starter(models.Model):
 
     column2 (str) : <可選> 自定義第二欄位 relation table 中 column 2 的名字
     """
+
     tag_ids = fields.Many2many("demo.tag", "", "", "", string="Tags")
+
+    """
+    tutorial 4 Many2Many
+    
+    使用 Selection 對應上方的 employee_id
+
+    1. fields.Selection 下拉選單 :
+    e.g. gender = fields.Selection(string="Gender", selection=[("a", "A"), ("b", "B")])
+
+    2. 特別的是 related : employee_id 從上面的 Mayn2one 對應到  "hr.employee" , 所以會去找裡面的 gender field
+    note : hr.employee 的 gender 欄位，屬性就是 Selection , 所以用 char 會報格式不對的 error
+
+    """
+
+    gender = fields.Selection(string="Gender", related="employee_id.gender")
+
+    """
+    API
+    """
 
     @api.depends("value")
     def _value_pc(self):
