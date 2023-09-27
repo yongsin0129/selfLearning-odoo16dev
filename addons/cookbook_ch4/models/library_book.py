433f3713af1298dbcc4556dfb5fdcf31aaf2102b
yongsin0129 <yongsin0129@gmail.com>
2023-09-27 15:51:47 +0800

    add ch4 向模型新增資料欄位

--- addons/cookbook_ch4/models/library_book.py
+++ addons/cookbook_ch4/models/library_book.py
@@ -6,15 +6,33 @@ class LibraryBook(models.Model):
     _description = 'Library Book'
     _order = 'date_release desc, name'
     _rec_name = "short_name"
-    
+
     name = fields.Char('Title', required=True)
-    short_name = fields.Char('Short Title', required=True)
-    date_release = fields.Date('Release Date')
     author_ids = fields.Many2many(
         'res.partner',
         string='Authors'
     )
+    state = fields.Selection(
+        [('draft', 'Not Available'),
+        ('available', 'Available'),
+        ('lost', 'Lost')],
+        'State')
+    pages = fields.Integer('Number of Pages')
+    notes = fields.Text('Internal Notes')
 
+    short_name = fields.Char('Short Title', required=True)
+    date_release = fields.Date('Release Date')
+    date_updated = fields.Datetime('Last Updated')
+    cover = fields.Binary('Book Cover')
+    reader_rating = fields.Float(
+        'Reader Average Rating',
+        digits=(14, 4), # Optional precision (total, decimals),
+    )
+
+    description = fields.Html('Description')
+
+    out_of_print = fields.Boolean('Out of Print?')
+    
     """本方法用于自定义记录的显示名称"""
     def name_get(self):
         result = []
