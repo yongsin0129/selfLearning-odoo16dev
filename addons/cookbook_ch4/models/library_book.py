ac87c3c15e5a1da38f55ff1c44791fd02824fb53
yongsin0129 <yongsin0129@gmail.com>
2023-09-27 18:09:04 +0800

    add ch4 向模型添加货币字段

--- addons/cookbook_ch4/models/library_book.py
+++ addons/cookbook_ch4/models/library_book.py
@@ -28,11 +28,22 @@ class LibraryBook(models.Model):
     )
 
     description = fields.Html("Description")
+    out_of_print = fields.Boolean("Out of Print?")
 
     # 3 使用可配置精度的浮点字段
     cost_price = fields.Float("Book Cost", digits="Book Price")
 
-    out_of_print = fields.Boolean("Out of Print?")
+    # 4 向模型添加货币字段
+    currency_id = fields.Many2one("res.currency", string="Currency")
+    retail_price = fields.Monetary(
+        "Retail Price",
+        # optional: currency_field='currency_id' (default),
+    )
+    # 同一頁面雙貨幣
+    currency_id1 = fields.Many2one("res.currency", string="Currency1")
+    currency_id2 = fields.Many2one("res.currency", string="Currency2")
+    retail_price1 = fields.Monetary("Retail Price1", currency_field="currency_id1")
+    retail_price2 = fields.Monetary("Retail Price2", currency_field="currency_id2")
 
     """本方法用于自定义记录的显示名称"""
 
