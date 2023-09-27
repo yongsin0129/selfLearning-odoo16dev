7f37fa53c81d07aadf4923233a7dc37d26fceb02
yongsin0129 <yongsin0129@gmail.com>
2023-09-27 15:39:45 +0800

    add ch4 定義模型表示及排序

--- addons/cookbook_ch4/models/library_book.py
+++ addons/cookbook_ch4/models/library_book.py
@@ -2,9 +2,23 @@ from odoo import api, fields, models
 
 
 class LibraryBook(models.Model):
-    # 1 模型
-    _name = "library.book"
-    _description = "Library Book"
+    _name = 'library.book'
+    _description = 'Library Book'
+    _order = 'date_release desc, name'
+    _rec_name = "short_name"
+    
+    name = fields.Char('Title', required=True)
+    short_name = fields.Char('Short Title', required=True)
+    date_release = fields.Date('Release Date')
+    author_ids = fields.Many2many(
+        'res.partner',
+        string='Authors'
+    )
 
-    name = fields.Char("Name", required=True)
-    date_release = fields.Date("Date_Release")
+    """本方法用于自定义记录的显示名称"""
+    def name_get(self):
+        result = []
+        for record in self:
+            rec_name = "%s (%s)" % (record.name, record.date_release)
+            result.append((record.id, rec_name))
+        return result
