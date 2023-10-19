404a1a14f83344124095089947308d371ddda6b8
yongsin0129 <yongsin0129@gmail.com>
2023-10-19 13:11:10 +0800

    feat : 1. Displaying a counter

--- addons/owl_playground/__manifest__.py
+++ addons/owl_playground/__manifest__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# pylint: skip-file
+
 {
     'name': "owl_playground",
 
@@ -33,13 +35,13 @@
             'web/static/lib/bootstrap/scss/_variables.scss',
             ('include', 'web._assets_bootstrap'),
 
-            'web/static/src/libs/fontawesome/css/font-awesome.css', # required for fa icons
-            'web/static/src/legacy/js/promise_extension.js', # required by boot.js
-            'web/static/src/boot.js', # odoo module system
-            'web/static/src/env.js', # required for services
-            'web/static/src/session.js', # expose __session_info__ containing server information
-            'web/static/lib/owl/owl.js', # owl library
-            'web/static/lib/owl/odoo_module.js', # to be able to import "@odoo/owl"
+            'web/static/src/libs/fontawesome/css/font-awesome.css',  # required for fa icons
+            'web/static/src/legacy/js/promise_extension.js',  # required by boot.js
+            'web/static/src/boot.js',  # odoo module system
+            'web/static/src/env.js',  # required for services
+            'web/static/src/session.js',  # expose __session_info__ containing server information
+            'web/static/lib/owl/owl.js',  # owl library
+            'web/static/lib/owl/odoo_module.js',  # to be able to import "@odoo/owl"
             'web/static/src/core/utils/functions.js',
             'web/static/src/core/browser/browser.js',
             'web/static/src/core/registry.js',
--- addons/owl_playground/controllers/controllers.py
+++ addons/owl_playground/controllers/controllers.py
@@ -1,6 +1,7 @@
 from odoo import http
 from odoo.http import request, route
 
+
 class OwlPlayground(http.Controller):
     @http.route(['/owl_playground/playground'], type='http', auth='public')
     def show_playground(self):
@@ -9,5 +10,9 @@ class OwlPlayground(http.Controller):
         """
         return request.render('owl_playground.playground')
 
-
-
+    @http.route(['/owl_playground/playground2'], type='http', auth='public')
+    def show_playground2(self):
+        """
+        Renders the owl playground page2
+        """
+        return request.render('owl_playground.playground2')
