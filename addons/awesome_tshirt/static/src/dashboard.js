8fc1b0c7a27df7c09f99628b3833490bd954a4b6
yongsin0129 <yongsin0129@gmail.com>
2023-10-19 15:50:42 +0800

    2.2 Add some buttons for quick navigation

--- addons/awesome_tshirt/static/src/dashboard.js
+++ addons/awesome_tshirt/static/src/dashboard.js
@@ -1,11 +1,61 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
-import { registry } from "@web/core/registry";
+import { registry } from "@web/core/registry"
+import { Layout } from "@web/search/layout"
+import { getDefaultConfig } from "@web/views/view"
+const { Component, useSubEnv } = owl
+import { useService } from "@web/core/utils/hooks"
+import { Domain } from "@web/core/domain"
 
-class AwesomeDashboard extends Component {}
+class AwesomeDashboard extends Component {
+  setup () {
 
-AwesomeDashboard.components = {};
-AwesomeDashboard.template = "awesome_tshirt.clientaction";
+    // The useSubEnv below can be deleted if you're > 16.0
+    useSubEnv({
+      config: {
+        ...getDefaultConfig(),
+        ...this.env.config,
+      },
+    })
 
-registry.category("actions").add("awesome_tshirt.dashboard", AwesomeDashboard);
+    this.display = {
+      controlPanel: { "top-right": false, "bottom-right": false },
+    }
+
+    this.action = useService("action")
+  }
+
+  openCustomerView () {
+    this.action.doAction("base.action_partner_form")
+  }
+
+  openOrders (title, domain) {
+    this.action.doAction({
+      type: "ir.actions.act_window",
+      name: title,
+      res_model: "awesome_tshirt.order",
+      domain: new Domain(domain).toList(),
+      views: [
+        [false, "list"],
+        [false, "form"],
+      ],
+    })
+  }
+
+  openLast7DaysOrders () {
+    const domain =
+      "[('create_date','>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d'))]"
+    this.openOrders("Last 7 days orders", domain)
+  }
+
+  openLast7DaysCancelledOrders () {
+    const domain =
+      "[('create_date','>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d')), ('state','=', 'cancelled')]"
+    this.openOrders("Last 7 days cancelled orders", domain)
+  }
+}
+
+AwesomeDashboard.components = { Layout }
+AwesomeDashboard.template = "awesome_tshirt.clientaction"
+
+registry.category("actions").add("awesome_tshirt.dashboard", AwesomeDashboard)
