a78607ff637c711736a97402bca7209d0a02a14e
yongsin0129 <yongsin0129@gmail.com>
2023-10-26 10:59:39 +0800

    6.6 Display images

--- addons/awesome_gallery/static/src/gallery_renderer.js
+++ addons/awesome_gallery/static/src/gallery_renderer.js
@@ -2,6 +2,9 @@
 
 const { Component } = owl
 
+import { GalleryImage } from "./gallery_image/gallery_image"
+
 export class GalleryRenderer extends Component { }
 
+GalleryRenderer.components = { GalleryImage }
 GalleryRenderer.template = "awesome_gallery.Renderer"
