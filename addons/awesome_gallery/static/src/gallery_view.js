cf0c8b5a8db65531d41e65ebc00c82e3986fc7ca
yongsin0129 <yongsin0129@gmail.com>
2023-10-26 10:04:15 +0800

    6.3 Parse the arch

--- addons/awesome_gallery/static/src/gallery_view.js
+++ addons/awesome_gallery/static/src/gallery_view.js
@@ -1,7 +1,9 @@
 /** @odoo-module */
 
 import { registry } from "@web/core/registry"
+
 import { GalleryController } from "./gallery_controller"
+import { GalleryArchParser } from "./gallery_arch_parser"
 
 export const galleryView = {
   type: "gallery",
@@ -9,6 +11,18 @@ export const galleryView = {
   icon: "fa fa-picture-o",
   multiRecord: true,
   Controller: GalleryController,
+  ArchParser: GalleryArchParser,
+
+  props (genericProps, view) {
+    const { ArchParser } = view
+    const { arch } = genericProps
+    const archInfo = new ArchParser().parse(arch)
+
+    return {
+      ...genericProps,
+      archInfo,
+    }
+  },
 }
 
 registry.category("views").add("gallery", galleryView)
