e5bedda3851ff63cb76e25f6b4ea15122563b0d8
yongsin0129 <yongsin0129@gmail.com>
2023-10-26 11:07:35 +0800

    6.7 Switch to form view on click

--- addons/awesome_gallery/static/src/gallery_image/gallery_image.js
+++ addons/awesome_gallery/static/src/gallery_image/gallery_image.js
@@ -2,11 +2,18 @@
 
 const { Component } = owl
 
-export class GalleryImage extends Component { }
+export class GalleryImage extends Component {
+
+  onClick () {
+    this.props.onClick(this.props.image.id)
+  }
+
+}
 
 GalleryImage.template = "awesome_gallery.GalleryImage"
 GalleryImage.props = {
   image: { type: Object },
   className: { type: String },
   imageField: { type: String },
+  onClick: { type: Function },
 }
